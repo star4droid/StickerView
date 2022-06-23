5bfdd5583773a8468821876065dcc0af7ebc5a6d
star4droid <81539602+star4droid@users.noreply.github.com>
2022-06-23 12:24:10 +0200

    Add files via upload

--- app/src/main/java/com/star4droid/StickerView/MainActivity.java
+++ app/src/main/java/com/star4droid/StickerView/MainActivity.java
@@ -1,1 +1,150 @@
+package com.star4droid.StickerView;
 
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
+
+public class MainActivity extends Activity {
+	
+	private  StickerView st;
+	
+	private FrameLayout linear1;
+	private TextView text;
+	private ImageView turntable;
+	private ImageView imageview1;
+	private ImageView imageview2;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		text = findViewById(R.id.text);
+		turntable = findViewById(R.id.turntable);
+		imageview1 = findViewById(R.id.imageview1);
+		imageview2 = findViewById(R.id.imageview2);
+		
+		turntable.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				st.select(turntable);
+			}
+		});
+		
+		imageview1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				st.select(imageview1);
+			}
+		});
+		
+		imageview2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				st.select(imageview2);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		st=new StickerView(this);
+		linear1.addView(st);
+		st.setOnEvent(new StickerView.onEvent(){
+			@Override 
+			public void onSizeChanged(int height,int width){
+				text.setText("height: "+height+" width: "+width);
+			}
+			@Override 
+			public void onClose(){
+				text.setText("Close Clicked");
+			}
+			@Override
+			public void onRotation(float rotation){
+				text.setText("rotation: "+rotation);
+			}
+		});
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
